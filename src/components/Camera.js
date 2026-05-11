f9303b6e3288e789e75c4ab95b50f471446f5358
taivan1805 <taivan1805@gmail.com>
2026-05-12 02:13:24 +0700

    fix: camera angle

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,23 +1,16 @@
 import * as THREE from "three";
 
 export function Camera() {
-  const size = 300;
-  const viewRatio = window.innerWidth / window.innerHeight;
-  const width = viewRatio < 1 ? size : size * viewRatio;
-  const height = viewRatio < 1 ? size / viewRatio : size;
-
-  const camera = new THREE.OrthographicCamera(
-    width / -2, // left
-    width / 2, // right
-    height / 2, // top
-    height / -2, // bottom
-    100, // near
-    900 // far
+  const aspect = window.innerWidth / window.innerHeight;
+  const camera = new THREE.PerspectiveCamera(
+    32,
+    aspect,
+    1, 
+    3000
   );
-
   camera.up.set(0, 0, 1);
-  camera.position.set(300, -300, 300);
-  camera.lookAt(0, 0, 0);
+  camera.position.set(180, -250, 280);
+  camera.lookAt(25, 25, 0);
 
   return camera;
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -12,6 +12,7 @@ export const metadata = [
       { tileIndex: -8, height: 30 },
       { tileIndex: -3, height: 50 },
       { tileIndex: 2, height: 30 },
+      { tileIndex: 5, height: 30 },
     ],
   },
   {
@@ -30,6 +31,7 @@ export const metadata = [
       { tileIndex: -5, height: 30 },
       { tileIndex: -1, height: 50 },
       { tileIndex: 3, height: 30 },
+      { tileIndex: 6, height: 30 },
     ],
   },
   {
@@ -42,11 +44,22 @@ export const metadata = [
     ],
   },
   {
+    type: "car",
+    direction: false,
+    speed: 188,
+    vehicles: [
+      { initialTileIndex: -4, color: 0xbdb638 },
+      { initialTileIndex: -1, color: 0x78b14b },
+      { initialTileIndex: 4, color: 0xa52523 },
+    ],
+  },
+  {
     type: "forest",
     trees: [
       { tileIndex: -8, height: 30 },
       { tileIndex: -3, height: 50 },
       { tileIndex: 2, height: 30 },
+      { tileIndex: 5, height: 30 },
     ],
   },
 ];
